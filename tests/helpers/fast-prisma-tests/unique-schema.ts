f9258b287816fe627525634f0b75d7e26e2dc3d2
Selim Belhaouane <selim.belhaouane@gmail.com>
2023-03-16 11:26:12 -0400

    build unique schema name based on jest worker id

--- tests/helpers/fast-prisma-tests/unique-schema.ts
+++ tests/helpers/fast-prisma-tests/unique-schema.ts
@@ -1,22 +1,12 @@
-import crypto from 'crypto'
-import path from 'path'
-
-import { PROJECT_DIR } from './constants'
-
-export const TEST_PATH = expect.getState().testPath
+const JEST_WORKER_ID_ENV = 'JEST_WORKER_ID'
 
 /**
- * Returns a (probably) unique postgres schema name for the current test file.
+ * Returns a unique postgres schema name for the current jest worker.
  */
 export function buildUniquePgSchemaName(): string {
-  if (!TEST_PATH) {
-    throw new Error('Missing test path.')
+  const workerId = process.env[JEST_WORKER_ID_ENV]
+  if (!workerId) {
+    throw new Error(`Missing '${JEST_WORKER_ID_ENV}'.`)
   }
-  const relpath = path.relative(PROJECT_DIR, TEST_PATH)
-  // Schema names are restricted to 63 characters
-  const lengthMax = 63
-  if (relpath.length <= lengthMax) return relpath
-  const pathHash = crypto.createHash('sha1').update(relpath).digest('hex')
-  const uniqueName = `${relpath}-${pathHash.slice(0, 8)}`
-  return uniqueName.slice(uniqueName.length - lengthMax)
+  return `jest-${workerId}`
 }
